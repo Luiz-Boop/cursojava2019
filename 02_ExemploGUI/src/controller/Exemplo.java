24e4bf119eddf63100fa738f4ea6fdf5d13d0397
Luiz Santos <58276004+Luiz-Boop@users.noreply.github.com>
2020-01-08 21:40:47 -0200

    Add files via upload

---
+++ 02_ExemploGUI/src/controller/Exemplo.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Exemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		javax.swing.JOptionPane.showMessageDialog(null, "SENAI", "EXEMPLO GUI", 1);
+		javax.swing.JOptionPane.showMessageDialog(null, "Quer aumento de salário", "Salário", javax.swing.JOptionPane.YES_NO_OPTION);
+		
+	}
+
+}
