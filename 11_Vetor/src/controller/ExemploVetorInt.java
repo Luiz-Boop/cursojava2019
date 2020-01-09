b98cd4463732752c01124003e58842e696e1cfbf
Luiz Santos <luizinhorodrigues.sd1@gmail.com>
2020-01-08 22:45:42 -0200

    mensagem

---
+++ 11_Vetor/src/controller/ExemploVetorInt.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class ExemploVetorInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numeros [] = new int [4];
+		numeros [0] = 6;
+		numeros [1] = 9;
+		numeros [2] = 17;
+		numeros [1] = 27;
+		// Definindo outro Num. para a posição "1", 
+		// é substituido como um ponto Inic. e ponto Fin.
+		System.out.println ("numeros [2] = " + numeros [2]);
+		System.out.println ("numeros [0] = " + numeros [0]);
+		System.out.println ("numeros [1] = " + numeros [1]);
+	}
+
+}
