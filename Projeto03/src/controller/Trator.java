bad8e25fba9a89bdd8317d6528282294306769b5
Luiz Santos <58276004+Luiz-Boop@users.noreply.github.com>
2020-02-12 21:15:36 -0300

    Add files via upload

---
+++ Projeto03/src/controller/Trator.java
@@ -0,0 +1,36 @@
+package controller;
+
+import model.Teclado;
+
+public class Trator {
+
+	static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	if(cavalos>100)
+
+	{
+		int cavalos = 100;
+		avaliacao_cavalos = "FORTE";
+	}else{
+
+		if (cavalos <= 100) {
+			int cavalos1 = 100;
+			avaliacao_cavalos = "FORTE";
+		}else{
+
+		}
+	}
+	private int quilometragem;
+	String avaliacao_cavalos;
+	int ano;
+	String cavalos;
+	{
+
+		System.out.println("O motor é de: " + cavalos);
+		System.out.println("O motor é de: " + cavalos);
+
+	}
+
+}}
